dda39b8662d23313ab7f9e3224fd87465e0a4b74
shivay10th <shivay9455@gmail.com>
2024-11-04 16:04:27 +0530

    Integrate Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import CustomEditor from "./components/CustomEditor/CustomEditor";
+import Home from './screens/Home';
 
 export const App = () => {
   return (
-    <>
-      <CustomEditor />
-    </>
+		<>
+			<Router>
+				<div>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/write">Write</Link>
+						</li>
+						<li>
+							<Link to="/blogs">My Blogs</Link>
+						</li>
+					</ul>
+
+
+          <Switch>
+            <Route  path='/'>
+              <Home/>
+            </Route>
+            <Route  path='/write'>
+              <CustomEditor/>
+            </Route>
+          </Switch> 
+				</div>
+
+				<CustomEditor />
+			</Router>
+		</>
   );
 };
